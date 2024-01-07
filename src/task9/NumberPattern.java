79e891dbd7d43418a3f2ff66d1df08088c46859f
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-07 17:12:29 +0530

    Update NumberPattern.java

--- src/task9/NumberPattern.java
+++ src/task9/NumberPattern.java
@@ -4,28 +4,27 @@ import java.util.Scanner;
 
 public class NumberPattern {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int n, number = 1;
+	int n, number = 1;
 		
-		Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
 		
-		System.out.println("Enter a Value: ");
-	    n = in.nextInt();
+	System.out.println("Enter a Value: ");
+	n = in.nextInt();
 		
         for(int i = 1; i <= n; i++) 
         {
         	
-            for (int j = 1; j <= i; j++) 
-            {
-                System.out.print(number + " ");
-                number++;
-            }
-            System.out.println();
-	      }
+        for (int j = 1; j <= i; j++) 
+        {
+        System.out.print(number + " ");
+         number++;
+        }
+        System.out.println();
 	}
-
+    }
 }
 ------------------------------------------------------------------------------------------------------------
 OUTPUT:
