b8d2e0fe953c7913dc287aaac1451f0a58ae4292
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-07 17:11:04 +0530

    Update ReverseString.java

--- src/task9/ReverseString.java
+++ src/task9/ReverseString.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class ReverseString {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String original, rev = "";
-		Scanner in = new Scanner(System.in);
+	String original, rev = "";
+	Scanner in = new Scanner(System.in);
 		
-		System.out.println("Enter a string to reverse: ");
-	    original = in.nextLine(); // calculates length of String
+	System.out.println("Enter a string to reverse: ");
+	original = in.nextLine(); // calculates length of String
 
-	    int length = original.length();
+	int length = original.length();
 
-	    for (int i = length - 1 ; i >= 0 ; i--)
-	      rev = rev + original.charAt(i);
+	for (int i = length - 1 ; i >= 0 ; i--)
+	rev = rev + original.charAt(i);
 
-	    System.out.println("Reverse of the string: " + rev);
+	System.out.println("Reverse of the string: " + rev);
 	    	   
-	       }
 	}
+}
 ---------------------------------------------------------------------------------------------------------------
 OUTPUT:
 Enter a string to reverse: 
