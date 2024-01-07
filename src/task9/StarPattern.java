a6d6c708fe6b23507b8ff2aba46c3858e8415363
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-07 17:10:21 +0530

    Update StarPattern.java

--- src/task9/StarPattern.java
+++ src/task9/StarPattern.java
@@ -4,31 +4,40 @@ import java.util.Scanner;
 
 public class StarPattern {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 		
-		int h;
+	int h;
 
         System.out.println("Enter the value: ");
         h = sc.nextInt();
 
         for (int i = 1; i < h + 1; i++) {
             
-        	for (int j = 1; j < h + 1; j++) {
+        for (int j = 1; j < h + 1; j++) {
                 
-        		if (i == j || i + j == h + 1) {
+        if (i == j || i + j == h + 1) {
                     
-        			System.out.print("*");
+        System.out.print("*");
                
-        		} else {
-                    
-        			System.out.print(" ");
-                }
-            }
+        } else {
+	System.out.print(" ");
+        }
+        }
            System.out.println();
         }
 	}
 
 }
+---------------------------------------------------------------------------------------------------------------
+OUTPUT:
+Enter the value: 
+5
+*   *
+ * * 
+  *  
+ * * 
+*   *
+
